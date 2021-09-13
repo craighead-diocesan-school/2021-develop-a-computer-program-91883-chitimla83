1966fdab23021cf692fcc34e1dd65fd84afbf6e5
chitimla83 <79882087+chitimla83@users.noreply.github.com>
2021-09-13 20:45:08 +1200

    added code
    tests
    
    names
     sara: passed
    jess : passed

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,25 +1,37 @@
 // Your code goes in this file.
 
-// ask for the user name 
-
-alert ("This is a guess the number game")
+// alert the name of the game  
+alert("This is a guess the number game")
 //ask for user name
 let username = prompt('Whats your name?')
+// allow the user input be equvilant to the numbers they have to guess
+let userNumber= 20
 // the variable is equal a radom number generator
-let ranNum = Math.floor(Math.random()* 20) + 1;
-// tells you the random number
-alert (ranNum)
 
+// generate a random number
+let ranNum = Math.floor(Math.random() * 20) + 1;
+// tells you the random number (remove later)
+alert(ranNum)
 
-// inform the purpose of the game and then ask the user to guess a number
-alert ( "I'm thinking of a number between 1- 20 ")
-let number = prompt ('guess a number')
+// ask the user to guess a number
+alert("I'm thinking of a number between 1- 20 ")
 // start a while statement and have two options, if and else
-while (number == ranNum ) {
-alert ('that is correct!')
-} if (number != ranNum) {
-    alert ('that is inocrrect')
+
+
+
+while (userNumber == ranNum) {
+  let userNumber = prompt('guess a number')
+  if (userNumber == ranNum) {
+    alert('that is correct!')
+  } else {
+    alert('that is incorrect')
+
+  }
 }
 
 
- 
+
+
+
+
+ // work on the three guesses, have two functions 
