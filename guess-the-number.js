172415297fbf9dd4ae6529c295f00fd4949bd7d8
chitimla83 <79882087+chitimla83@users.noreply.github.com>
2021-09-15 14:43:02 +1200

    code fufills task
    expected value test:
    "14" passed
    "6" passed
    "7" passed
    
    boundary value test:
    "1" passed
    "20" passed
    
    invalid value tests:
    jgdshjvds: failed
    hi: failed

--- guess-the-number.js
+++ guess-the-number.js
@@ -4,32 +4,37 @@
 alert("This is a guess the number game")
 //ask for user name
 let username = prompt('Whats your name?')
-// allow the user input be equvilant to the numbers they have to guess
-let userNumber= 20
-// the variable is equal a radom number generator
 
 // generate a random number
 let ranNum = Math.floor(Math.random() * 20) + 1;
+
 // tells you the random number (remove later)
 alert(ranNum)
 
 // ask the user to guess a number
 alert("I'm thinking of a number between 1- 20 ")
-// start a while statement and have two options, if and else
+
+// validate and make sure uername is name and number is a number
+let guess = 0
 
 
-
-while (userNumber == ranNum) {
+while (guess != 3) {
+  // add one to the thier the guesses so that the loop stops at 3
+  guess += 1
+  //ask for user input
   let userNumber = prompt('guess a number')
+  //convert user input to a number
+  userNumber = Number(userNumber)
+// check the suer answer 
   if (userNumber == ranNum) {
-    alert('that is correct!')
+    alert('that is correct')
+  // ask if you should put another if else statemnet in here to alert "do you wnat to play another round of the game "
+  
   } else {
-    alert('that is incorrect')
 
+    alert('that is incorrect')
   }
 }
-
-
 
 
 
